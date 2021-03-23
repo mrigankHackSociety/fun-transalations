3bf8749edbef9960ced98516d884523774e68cae
mrigankchou@gmail.com <mrigankchou@gmail.com>
2021-03-24 00:04:14 +0530

    development(): transalation json added

---
+++ src/transalations.js
@@ -0,0 +1,116 @@
+export const movieTransalationsArray = [
+    {
+        movieName: "Starwars",
+        transalations: [
+            {
+                name: "Yoda",
+                value: "yoda",
+            },
+            {
+                name: "Sith",
+                value: "sith",
+            },
+            {
+                name: "Gungan",
+                value: "gungan",
+            },
+            {
+                name: "Huttese",
+                value: "huttese",
+            },
+            {
+                name: "Mandalorian",
+                value: "mandalorian",
+            },
+            {
+                name: "Cheunh",
+                value: "cheunh",
+            },
+            {
+                name: "Aurebesh",
+                value: "aurebesh",
+            },
+        ],
+    },
+    {
+        movieName: "Game Of Thrones",
+        transalations: [
+            {
+                name: "Dothraki",
+                value: "dothraki",
+            },
+            {
+                name: "Valyrian",
+                value: "valyrian",
+            },
+            {
+                name: "Hodor",
+                value: "hodor",
+            },
+        ],
+    },
+    {
+        movieName: "Marvel",
+        transalations: [
+            {
+                name: "Wakandan",
+                valiue: "wakandan",
+            },
+            {
+                name: "Groot",
+                valiue: "groot",
+            },
+            {
+                name: "Doop",
+                valiue: "doop",
+            },
+        ],
+    },
+    {
+        movieName: "Star Trek",
+        transalations: [
+            {
+                name: "Vulcan",
+                valiue: "vulcan",
+            },
+            {
+                name: "Klingon",
+                valiue: "klingon",
+            },
+            {
+                name: "Romulan",
+                valiue: "romulan",
+            },
+        ],
+    },
+    {
+        movieName: "Elvish",
+        transalations: [
+            {
+                name: "Sindarin",
+                valiue: "sindarin",
+            },
+            {
+                name: "Quenya",
+                valiue: "quenya",
+            },
+            {
+                name: "Tengwar",
+                valiue: "tengwar",
+            },
+            {
+                name: "Orcish",
+                valiue: "orcish",
+            },
+        ],
+    },
+    {
+        movieName: "Despicable me",
+        transalations: [
+            {
+                name: "Minions",
+                value: "minions",
+            },
+        ],
+    },
+];
